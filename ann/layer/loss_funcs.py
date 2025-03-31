0246053f5fee0277bf499b5a51123bfba7e6bdc9
KienQuoc52withVeolaohtihn <quoc.tran522005@hcmut.edu.vn>
2025-03-31 17:55:07 +0700

    Update loss_funcs.py

--- ann/layer/loss_funcs.py
+++ ann/layer/loss_funcs.py
@@ -2,22 +2,13 @@ import numpy as np
 
 
 def mse(y_true, y_pred):
-    # TODO
 
 
 def mse_derivative(y_true, y_pred):
-    # TODO: return the vector dE/dY
 
 
-def CCE(y_true, y_pred):  # Categorical Cross-entropy
-    # TODO
-    # Since the labels will be one-hot encode so we need to clip predictions
-    # in the range [1e-12, 1 - 1e-12] to prevent log(0)
-    # e.g. [0.1, 0.1, 0.0, 0.8] which will result in log(0)
-   
-    # NOTE: should pay attention to training in batches instead of training in single image
+def CCE(y_true, y_pred):  
 
 
 def CCE_derivative(y_true, y_pred):
-    # TODO
-    return -np.array(y_true) / y_pred  # TODO: check for error, output_gradient (4x4)
+    return -np.array(y_true) / y_pred  
