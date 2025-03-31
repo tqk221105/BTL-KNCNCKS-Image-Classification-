3e44652c9f4e2726a87c133f4bf4e37e67c58b91
KienQuoc52withVeolaohtihn <quoc.tran522005@hcmut.edu.vn>
2025-03-31 17:53:17 +0700

    Update FCLayer.py

--- ann/layer/FCLayer.py
+++ ann/layer/FCLayer.py
@@ -1,22 +1,4 @@
-"""
-    w_ij
-x_i -----> y_j = w_1j.x_1 + w_2j.x_2 +...+ w_ij.x_i + b_j
 
-
-    y_1        w_11  w_12  ...  w_1i         x_1           b_1
-    y_2        w_21  w_22  ...  w_2i         x_2           b_2
-    .     =   .    .    .    .     x     .      +     .
-    .         .    .     .   .           .            .
-    .         .    .      .  .           .            .
-    y_j        w_j1  w_j2  ...  w_ji         x_i           b_j
-
-    Y = W x X + B
-
-Y(j x 1)
-W(j x i)
-X(i x 1)
-B(j x 1)
-"""
 
 
 import numpy as np
@@ -29,10 +11,8 @@ class FCLayer(ILayer):
         
 
     def forward(self, X):
-        # TODO: return output Y
         
 
     def backward(self, output_gradient, learning_rate):
-                        # dE/dY(j x 1)
-        # TODO: update Weight and Bias, then return input gradient X_gradient
+                       
        
