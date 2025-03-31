6d4ca155112387010aafc74f6cd794ff0d80e622
KienQuoc52withVeolaohtihn <quoc.tran522005@hcmut.edu.vn>
2025-03-31 17:47:52 +0700

    Update activation_funcs.py

--- ann/layer/activation_funcs.py
+++ ann/layer/activation_funcs.py
@@ -7,39 +7,23 @@ from ann.layer.activation import Activation
 class Softmax(ILayer):
     def forward(self, input):
         # TODO
-        self.input = input
+        
         # Subtract the number with the largest number to prevent the exp too large
-        temp = np.exp(self.input - np.max(self.input))
-        self.output = temp / np.sum(temp)
-        return self.output
-
+        
     def backward(self, output_gradient, learning_rate):
         # TODO
-        temp = self.output.reshape(-1, 1)  # Convert to column vector
-        jacobian = np.diagflat(temp) - np.dot(temp, np.transpose(temp))
-
-        return np.dot(jacobian, output_gradient)
+         # Convert to column vector
+        
 
 
 class Sigmoid(Activation):
     def __init__(self):
         # TODO
         def sigmoid(x):
-            return 1 / (1 + np.exp(-x))
 
         def sigmoid_derivative(x):
             # TODO
-            """
-            let u = 1 - np.exp(-x)
-            and f'(x) = -np.exp(-x) / [(1 - np.exp(-x)) ** 2]
-            ==> f'(x) = (u - 1) / u^2 = (1/u) * [1 - (1/u)]
-            but 1/u = sigmoid(x)
-            therefore,
-            """
-            s = sigmoid(x)
-            return s * (1 - s)
-
-        super().__init__(sigmoid, sigmoid_derivative)
+            
 
 
 class ReLU(Activation):
