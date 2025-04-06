4626b7464934ea098d1e5fdd3c8658afaa8466f6
tqk221105 <khanh.trantqk221105@hcmut.edu.vn>
2025-04-06 23:29:05 +0700

    reshape.py

--- ann/layer/reshape.py
+++ ann/layer/reshape.py
@@ -1,3 +1,4 @@
+'''
 import numpy as np
 from ann.layer.ILayer import ILayer
 
@@ -9,3 +10,19 @@ class Reshape(ILayer):
     def forward(self, X):
 
     def backward(self, output_gradient, learning_rate):
+'''
+
+import numpy as np
+from ann.layer.ILayer import ILayer
+
+class Reshape(ILayer):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward(self, X):
+        self.input_data = X
+        return X.reshape(self.output_shape)
+
+    def backward(self, output_gradient, learning_rate):
+        return output_gradient.reshape(self.input_shape)
