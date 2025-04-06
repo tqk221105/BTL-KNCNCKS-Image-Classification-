2add4954f9871bc6ac0553ae0913af71c0044cf4
tqk221105 <khanh.trantqk221105@hcmut.edu.vn>
2025-04-06 23:21:38 +0700

    activation.py

--- ann/layer/activation.py
+++ ann/layer/activation.py
@@ -4,11 +4,13 @@ from ann.layer.ILayer import ILayer
 
 class Activation(ILayer):
     def __init__(self, activation, activation_derivative):
-       
-        
+        self.activation = activation
+        self.activation_derivative = activation_derivative
 
     def forward(self, input):
-       
+        self.input = input 
+        return self.activation(input)
 
     def backward(self, output_gradient, learning_rate):
-                        
+        input_gradient = output_gradient * self.activation_derivative(self.input)
+        return input_gradient
