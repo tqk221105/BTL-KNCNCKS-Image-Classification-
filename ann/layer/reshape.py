d39f74dc45a0a0a562a61e49d6b02b5c07e203c3
tqk221105 <khanh.trantqk221105@hcmut.edu.vn>
2025-04-08 14:32:07 +0700

    reshape.py

--- ann/layer/reshape.py
+++ ann/layer/reshape.py
@@ -1,11 +1,18 @@
 import numpy as np
 from ann.layer.ILayer import ILayer
 
-
 class Reshape(ILayer):
     def __init__(self, input_shape, output_shape):
-        
+        super().__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.input = None
 
     def forward(self, X):
+        self.input = X
+        batch_size = X.shape[0]
+        return X.reshape((batch_size, *self.output_shape))
 
     def backward(self, output_gradient, learning_rate):
+        batch_size = output_gradient.shape[0]
+        return output_gradient.reshape((batch_size, *self.input_shape))
